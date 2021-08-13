ddc8eb2a2bf6598d4ada48b059b12afd34ddd14e
nugeat23 <nugeat23@gmail.com>
2021-08-13 11:14:28 +0900

    ex01.cpp 추가

---
+++ chapter01/ex01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const  *argv[])
+
+{
+
+    cout << "Hello world" << endl;
+    // std::cout << "Hello World" << std::endl;
+
+    return 0;           // 파이썬 sys.exit(0)
+
+
+}
+

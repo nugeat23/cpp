fdabc0a8b3f0463c2b63cd6a338780baf925e6f9
nugeat23 <nugeat23@gmail.com>
2021-08-16 09:38:10 +0900

    Chapter02 완성

---
+++ chapter02/ex05.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+
+    int x = 100.12;
+
+    if (x==100){
+
+        cout << "x가 100입니다." << endl;
+        
+    }
+
+    else{
+
+        cout << "x가 100이 아닙니다." << endl;
+
+    }
+
+
+}
